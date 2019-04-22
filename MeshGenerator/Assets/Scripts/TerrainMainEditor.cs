3f3f2202a9592c80b8d83ed8ea1c2a83f318f779
liutao <2809832659@qq.com>
2019-04-22 22:39:36 +0800

    格子

--- MeshGenerator/Assets/Scripts/TerrainMainEditor.cs
+++ MeshGenerator/Assets/Scripts/TerrainMainEditor.cs
@@ -60,5 +60,55 @@ namespace MaybeInside
 			UnityEditor.AssetDatabase.CreateAsset(mesh, path);
 #endif
 		}
+
+		[EditorButton]
+		private void CreateGridMesh(int width, int height, string path)
+		{
+			var cellSize = new Vector3(1f, 0f, 1f);
+
+			var leftBottom = new Vector3(
+				-(cellSize.x * width * 0.5f),
+				0f,
+				-(cellSize.z * height * 0.5f)
+			);
+			var vertices = new Vector3[(width + 1) * (1 + height)];
+			for (int i = 0; i <= height; i++)
+			{
+				for (int j = 0; j <= width; j++)
+				{
+					var x = leftBottom.x + cellSize.x * j;
+					var z = leftBottom.z + cellSize.z * i;
+					vertices[i * height + j + i] = new Vector3(x, 0f, z);
+				}
+			}
+
+			var lines = new int[(width + 1) * (height + 1) * 2];
+			int trianglesIndex = 0;
+			for (int i = 0; i <= height; i++)
+			{
+				int start = i * (width + 1);
+				lines[trianglesIndex] = start;
+				trianglesIndex++;
+				lines[trianglesIndex] = start + width;
+				trianglesIndex++;
+			}
+
+			for (int i = 0; i <= width; i++)
+			{
+				lines[trianglesIndex] = i;
+				trianglesIndex++;
+				lines[trianglesIndex] = i + (width + 1) * height;
+				trianglesIndex++;
+			}
+
+			var mesh = new Mesh
+			{
+				vertices = vertices,
+			};
+			mesh.SetIndices(lines, MeshTopology.Lines, 0);
+#if UNITY_EDITOR
+			UnityEditor.AssetDatabase.CreateAsset(mesh, path);
+#endif
+		}
 	}
 }
